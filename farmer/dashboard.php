4055424516bdb240d2dab24a92c8064ffd9f0307
ThilinaVP <thilinavp@gmail.com>
2021-10-02 13:37:59 +0530

    farmer dashboard

---
+++ farmer/dashboard.php
@@ -0,0 +1,105 @@
+<html>
+<head>
+    <link rel="stylesheet" href="../css/style.css" type="text/css">   
+</head>
+<body>
+    <div id="dashboard">
+        <div id="profile-info-card">
+            <div class="profile-upper">
+                <div class = "profile-pic">
+                    <img src="pp.jpg" alt="">
+                </div>
+                <h1>Hello</h1>
+                <h2>Thilina Peduruhewa</h2>
+            </div>
+            <table id="farmer-card-stats">
+                <tbody>
+                    <tr>
+                        <td class="stats-desc">Orders completed</td>
+                        <td class="stat">50</td>
+                    </tr>
+                    <tr>
+                        <td class="stats-desc">Orders completed</td>
+                        <td class="stat">231</td>
+                    </tr>
+                    <tr>
+                        <td class="stats-desc">Orders completed</td>
+                        <td class="stat">12</td>
+                    </tr>
+                    <tr>
+                        <td class="stats-desc rating">Rating</td>
+                        <td class="stat"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="card-line">
+            <div class="dash-card">
+                <h1>Add stock</h1>
+                <img src="../assets/stock.png" alt="">
+            </div>
+            <div class="dash-card">
+                <h1>Offers Sentk</h1>
+                <img src="../assets/stock.png" alt="">
+            </div>
+            <div class="dash-card">
+                <h1>Earnings</h1>
+                <img src="../assets/stock.png" alt="">
+            </div>
+        </div>
+        <div class="card-line">
+            <div class="dash-card">
+                <h1>Buyer Requests</h1>
+                <img src="../assets/stock.png" alt="">
+            </div>
+            <div class="dash-card">
+                <h1>Contact Admins</h1>
+                <img src="../assets/stock.png" alt="">
+            </div>
+            <div class="dash-card">
+                <h1>Analytics</h1>
+                <img src="../assets/stock.png" alt="">
+            </div>
+        </div>
+        <div id="order-table">
+        <h1>current orders</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Order ID</th>
+                    <th>Delivery Date</th>
+                    <th>Description</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>001</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>001</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>001</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>001</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+    </div>
+</body>
+</html>
